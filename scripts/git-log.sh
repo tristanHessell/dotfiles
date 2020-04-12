2931ee6fa89c195542274532dd77b8cb5c741cee
thessell <tristan.hessell@gmail.com>
2020-04-12 14:44:34 +1000

    add interactive log to git

---
+++ scripts/git-log.sh
@@ -0,0 +1,18 @@
+  git log \
+    --graph --color=always --abbrev=7 --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" $@ | \
+    fzf \
+      --ansi --no-sort --reverse --tiebreak=index \
+      --preview "f() { set -- \$(echo -- \$@ | grep -o '[a-f0-9]\{7\}'); [ \$# -eq 0 ] || git show --color=always \$1 -- $@; }; f {}" \
+      --bind=ctrl-d:preview-page-down \
+      --bind=ctrl-u:preview-page-up \
+      --bind="enter:execute:
+          (grep -o '[a-f0-9]\{7\}' | head -1 |
+          xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+          {}
+          FZF-EOF"
+
+# TODO
+# toggle preview
+# copy commit
+# enter takes you to vim diff
+
